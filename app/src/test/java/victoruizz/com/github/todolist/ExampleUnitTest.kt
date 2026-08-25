4ee1d94f9498063c289b2cfba28ca1d05566b4e6
Victor Ruiz <ruizvictor077@outlook.com>
2026-08-25 20:13:30 -0300

    feat: first commit

---
+++ app/src/test/java/victoruizz/com/github/todolist/ExampleUnitTest.kt
@@ -0,0 +1,12 @@
+package victoruizz.com.github.todolist
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
