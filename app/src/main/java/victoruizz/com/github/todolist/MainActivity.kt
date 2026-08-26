80507f081f231101394424b7952ba7236760629d
Victor Ruiz <ruizvictor077@outlook.com>
2026-08-26 08:52:47 -0300

    feat: Integrando AppNavigation e TarefaViewModel na MainActivity

--- app/src/main/java/victoruizz/com/github/todolist/MainActivity.kt
+++ app/src/main/java/victoruizz/com/github/todolist/MainActivity.kt
@@ -4,14 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import victoruizz.com.github.todolist.navigation.AppNavigation
 import victoruizz.com.github.todolist.ui.theme.TodolistTheme
+import victoruizz.com.github.todolist.viewmodel.TarefaViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +15,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TodolistTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val viewModel: TarefaViewModel = viewModel(
+                    factory = TarefaViewModel.factory(applicationContext)
+                )
+                AppNavigation(viewModel = viewModel)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TodolistTheme {
-        Greeting("Android")
     }
 }
